3492777768ce940135afe18b2d40be6550126e53
sanjeet kumar <skc0845@gmail.com>
2023-03-05 02:53:54 +0530

    modified

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-export default [
+const data = [
     {
         id: 1,
         title: "Mount Fuji",
@@ -13,6 +13,7 @@ export default [
         title: "Sydney Opera House",
         location: "AUSTRALIA",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+
         date: "27 May, 2021 - 8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
         imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
@@ -27,3 +28,4 @@ export default [
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     }
 ]
+export default data;
